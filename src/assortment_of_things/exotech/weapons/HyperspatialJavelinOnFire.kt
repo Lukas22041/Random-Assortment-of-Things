22da1c3dd3f2a1d1c85bcb2ea7f9193986e581e3
Lukas22041 <lukas.kurth04@gmail.com>
2023-12-20 01:12:33 +0100

    b

--- src/assortment_of_things/exotech/weapons/HyperspatialJavelinOnFire.kt
+++ src/assortment_of_things/exotech/weapons/HyperspatialJavelinOnFire.kt
@@ -4,10 +4,20 @@ import com.fs.starfarer.api.combat.CombatEngineAPI
 import com.fs.starfarer.api.combat.DamagingProjectileAPI
 import com.fs.starfarer.api.combat.OnFireEffectPlugin
 import com.fs.starfarer.api.combat.WeaponAPI
+import org.lazywizard.lazylib.MathUtils
+import org.lazywizard.lazylib.ext.plus
+import org.lwjgl.util.vector.Vector2f
+import java.awt.Color
 
 class HyperspatialJavelinOnFire : OnFireEffectPlugin {
     override fun onFire(projectile: DamagingProjectileAPI, weapon: WeaponAPI?, engine: CombatEngineAPI) {
         var target = weapon?.ship?.shipTarget
         engine.addPlugin(HyperspatialJavelinHomingEffect(projectile, target))
+       // engine.spawnExplosion(Vector2f(projectile.location), Vector2f(weapon!!.ship.velocity), Color(252,143,0, 25), 100f, 2f)
+        for (i in 0..20) {
+            engine.addNebulaParticle(MathUtils.getRandomPointOnCircumference(Vector2f(projectile.location), MathUtils.getRandomNumberInRange(0f, 10f)),
+                MathUtils.getRandomPointOnCircumference(Vector2f(weapon!!.ship.velocity), MathUtils.getRandomNumberInRange(20f, 40f)),
+                MathUtils.getRandomNumberInRange(2f, 20f), 0f, 1f, 1f, 1f, Color(252,143,0, 100))
+        }
     }
 }
