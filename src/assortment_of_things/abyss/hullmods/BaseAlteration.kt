b778d31d2ba547be3e28dc266853c2f9c364f01f
Lukas22041 <lukas.kurth04@gmail.com>
2024-01-19 18:47:24 +0100

    stuff

--- src/assortment_of_things/abyss/hullmods/BaseAlteration.kt
+++ src/assortment_of_things/abyss/hullmods/BaseAlteration.kt
@@ -1,13 +1,17 @@
 package assortment_of_things.abyss.hullmods
 
+import com.fs.starfarer.api.campaign.CampaignUIAPI
 import com.fs.starfarer.api.campaign.econ.MarketAPI
 import com.fs.starfarer.api.combat.BaseHullMod
 import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.combat.ShipVariantAPI
 import com.fs.starfarer.api.fleet.FleetMemberAPI
 import com.fs.starfarer.api.ui.TooltipMakerAPI
+import java.awt.Color
 
 abstract class BaseAlteration : BaseHullMod() {
+
+    var color = Color(252, 3, 98)
 
     open fun canInstallAlteration(member: FleetMemberAPI?, variant: ShipVariantAPI?, marketAPI: MarketAPI?) : Boolean {
         return true
@@ -29,11 +33,21 @@ abstract class BaseAlteration : BaseHullMod() {
 
     }
 
-
     override fun isApplicableToShip(ship: ShipAPI?): Boolean {
         return false
     }
 
+    override fun canBeAddedOrRemovedNow(ship: ShipAPI?, marketOrNull: MarketAPI?, mode: CampaignUIAPI.CoreUITradeMode?): Boolean {
+        return false
+    }
+
+    override fun getBorderColor(): Color {
+        return color
+    }
+
+    override fun getNameColor(): Color {
+        return color
+    }
 
     override fun getUnapplicableReason(ship: ShipAPI?): String {
         return "Alterations can only be installed through the associated item."
