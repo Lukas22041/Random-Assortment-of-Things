60a6b2b4b233d3724f56ca509137d29ca6109624
Lukas22041 <lukas.kurth04@gmail.com>
2024-01-24 16:21:36 +0100

    a

--- src/assortment_of_things/abyss/hullmods/abyssals/AbyssalsAdaptabilityHullmod.kt
+++ src/assortment_of_things/abyss/hullmods/abyssals/AbyssalsAdaptabilityHullmod.kt
@@ -130,7 +130,7 @@ class AbyssalsAdaptabilityHullmod : BaseHullMod() {
             stats!!.variant.removeTag(Tags.SHIP_LIMITED_TOOLTIP)
         }
 
-        if (!stats!!.variant.hasHullMod("rat_abyssal_conversion") && !stats!!.variant.hasHullMod("rat_chronos_conversion") && !stats!!.variant.hasHullMod("rat_cosmos_conversion") && !stats.variant.hasHullMod(HullMods.AUTOMATED))
+        if (!stats!!.variant.hasHullMod("rat_abyssal_conversion") && !stats!!.variant.hasHullMod("rat_chronos_conversion") && !stats!!.variant.hasHullMod("rat_cosmos_conversion") && !stats!!.variant.hasHullMod("rat_seraph_conversion") && !stats.variant.hasHullMod(HullMods.AUTOMATED))
         {
             stats.variant.addPermaMod(HullMods.AUTOMATED)
         }
--- src/assortment_of_things/abyss/hullmods/abyssals/GenesisHullmod.kt
+++ src/assortment_of_things/abyss/hullmods/abyssals/GenesisHullmod.kt
@@ -1,22 +1,30 @@
 package assortment_of_things.abyss.hullmods.abyssals
 
+import assortment_of_things.abyss.AbyssUtils
 import assortment_of_things.abyss.hullmods.HullmodTooltipAbyssParticles
+import assortment_of_things.combat.AfterImageRenderer
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.combat.BaseHullMod
 import com.fs.starfarer.api.combat.MutableShipStatsAPI
 import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.impl.campaign.ids.HullMods
 import com.fs.starfarer.api.impl.campaign.ids.Skills
 import com.fs.starfarer.api.impl.campaign.ids.Stats
 import com.fs.starfarer.api.impl.campaign.ids.Tags
 import com.fs.starfarer.api.ui.TooltipMakerAPI
+import com.fs.starfarer.api.util.IntervalUtil
 import com.fs.starfarer.api.util.Misc
 import lunalib.lunaExtensions.addLunaElement
+import org.lazywizard.lazylib.ext.plus
+import org.lwjgl.util.vector.Vector2f
+import org.magiclib.kotlin.setAlpha
 
-class AbyssalGrid : BaseHullMod() {
+class GenesisHullmod : BaseHullMod() {
 
+    var color = AbyssUtils.GENESIS_COLOR
+    var afterimageInterval = IntervalUtil(0.1f, 0.1f)
 
     override fun applyEffectsBeforeShipCreation(hullSize: ShipAPI.HullSize?, stats: MutableShipStatsAPI?, id: String?) {
-
 
         if (Global.getSector()?.characterData?.person != null) {
             if (Global.getSector().characterData.person!!.stats.hasSkill(Skills.AUTOMATED_SHIPS)
@@ -29,11 +37,16 @@ class AbyssalGrid : BaseHullMod() {
             }
         }
 
-        stats!!.energyWeaponFluxCostMod.modifyMult(id, 0.9f)
-        stats!!.energyWeaponRangeBonus.modifyFlat(id, 100f)
+        if (!stats!!.variant.hasHullMod("rat_abyssal_conversion") && !stats!!.variant.hasHullMod("rat_chronos_conversion") && !stats!!.variant.hasHullMod("rat_cosmos_conversion") && !stats!!.variant.hasHullMod("rat_seraph_conversion")  && !stats.variant.hasHullMod(
+                HullMods.AUTOMATED)) {
+            stats.variant.addPermaMod(HullMods.AUTOMATED)
+        }
+
+        stats!!.energyWeaponFluxCostMod.modifyMult(id, 0.8f)
+        stats!!.energyWeaponRangeBonus.modifyFlat(id, 200f)
         //stats!!.beamWeaponRangeBonus.modifyMult(id, 100f)
 
-        stats.empDamageTakenMult.modifyMult(id, 0.75f)
+        stats.empDamageTakenMult.modifyMult(id, 0.5f)
 
         stats!!.getDynamic().getStat(Stats.CORONA_EFFECT_MULT).modifyMult(id, 0f);
     }
@@ -42,23 +55,34 @@ class AbyssalGrid : BaseHullMod() {
         return false
     }
 
+    override fun advanceInCombat(ship: ShipAPI?, amount: Float) {
+        afterimageInterval.advance(Global.getCombatEngine().elapsedInLastFrame)
+        if (afterimageInterval.intervalElapsed() && !Global.getCombatEngine().isPaused)
+        {
+            AfterImageRenderer.addAfterimage(ship!!, color.setAlpha(75), color.setAlpha(0), 3.5f, 0f, Vector2f().plus(ship!!.location))
+        }
+
+       // ship!!.setJitter(this, color.setAlpha(25), 1f, 3, 0f, 0f)
+        //ship!!.setJitterUnder(this, color.setAlpha(75), 1f, 25, 0f, 14f)
+    }
+
     override fun addPostDescriptionSection(tooltip: TooltipMakerAPI?, hullSize: ShipAPI.HullSize?, ship: ShipAPI?,  width: Float, isForModSpec: Boolean) {
 
         var initialHeight = tooltip!!.heightSoFar
-        var particleSpawner = HullmodTooltipAbyssParticles(tooltip, initialHeight)
+        var particleSpawner = HullmodTooltipAbyssParticles(tooltip, initialHeight, color.brighter())
         var element = tooltip!!.addLunaElement(0f, 0f).apply {
             advance { particleSpawner.advance(this, it) }
             render { particleSpawner.renderBelow(this, it) }
         }
 
         tooltip!!.addSpacer(5f)
-        tooltip.addPara("This ships flux grid is highly optimised for the use of energy weapons. " +
-                "This allows them to operate at a 10%% lower flux cost than normal and increases their base range by 100 units. " +
+        tooltip.addPara("A ship thats warping the space around it just by its sheer existance. Enables the \"Primordial Sea\" subsystem, opening a rift towards locations yet unseen.\n\n" +
+                "Energy weapons operate at a 20%% lower flux cost, have an increased base range of 200 units and cost 2/4/6 less ordnance points based on mount size." +
                 "\n\n" +
-                "This unique grid can also absorb the impact of charged particles, enabling it to resist emp damage up to 25%% better than other hulls and provides an immunity against abyssal storms and similar hazards." +
+                "The ships distorted grid takes 50%% less emp damage and has full immunity against abyssal storms and similar hazards." +
                 "",
             0f, Misc.getTextColor(), Misc.getHighlightColor(),
-            "energy weapons", "10%", "100", "emp", "25%", "abyssal storms")
+            "\"Primordial Sea\"","Energy weapons", "20%", "200", "2", "4", "6", "50%", "abyssal storms")
 
 
 
