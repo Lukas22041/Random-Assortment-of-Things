c12497ac2382c3d310a5cfb04b605570497daa7c
Lukas22041 <lukas.kurth04@gmail.com>
2024-07-26 06:04:35 +0200

    aaaaaaaaaaaaa

--- src/assortment_of_things/exotech/shipsystems/ai/HypatiaSystemAI.kt
+++ src/assortment_of_things/exotech/shipsystems/ai/HypatiaSystemAI.kt
@@ -17,26 +17,32 @@ class HypatiaSystemAI : ShipSystemAIScript {
     var previousAssignment: AssignmentInfo? = null
 
     //However clear the assignment after a while, just in case
-    var clearAssignmentTimeMax = 40f
+    var clearAssignmentTimeMax = 30f
     var clearAssignmentTime = clearAssignmentTimeMax
 
     //Need to check if the advance in phase stays correct
-    var maximumWarpTime = 25f
+    var maximumWarpTime = 35f
     var warpTime = maximumWarpTime
 
     //Actual target to keep track off for the disabling of the system
     var targetEntity: CombatEntityAPI? = null
 
     //Decent difference to make ships do actions less synchronosely
-    var interval = IntervalUtil(0.2f, 1.25f)
+    var inactiveInterval = IntervalUtil(1f, 3f)
+    var activeInterval = IntervalUtil(0.1f, 0.2f)
 
     var minDistanceNonAssignmentWarp = 3500
     var minDistanceAssignmentWarp = 3500
-    var distanceForUnwarp = 1600f
+    var distanceForUnwarp = 1500f
 
     var reachedTarget = false
 
     var previousTarget: CombatEntityAPI? = null
+
+    //Randomise a position around the target to fly to each time, avoids placing all ships in the same end location
+   /* var radialOffset = MathUtils.getRandomNumberInRange(-35f, 35f)
+    var distanceOffset = MathUtils.getRandomNumberInRange(200f, 800f)
+    var angleAtStart = 0f*/
 
     override fun init(ship: ShipAPI?, system: ShipSystemAPI?, flags: ShipwideAIFlags?, engine: CombatEngineAPI?) {
         this.ship = ship
@@ -54,25 +60,35 @@ class HypatiaSystemAI : ShipSystemAIScript {
         if (clearAssignmentTime <= 0f) {
             clearAssignmentTime = clearAssignmentTimeMax
             //previousAssignment = null
+
             previousTarget = null
+        }
+
+        //Reset previous target if its really far away
+        if (previousTarget != null) {
+            var distance = MathUtils.getDistance(ship, previousTarget)
+            if (distance >= minDistanceAssignmentWarp * 1.5f) {
+                previousTarget = null
+                clearAssignmentTime = clearAssignmentTimeMax
+            }
         }
 
         //Dont need to do any more if within those states
         if (ship!!.system.isCoolingDown || ship!!.system.isChargedown || ship!!.system.isChargeup) return
 
-        interval.advance(amount)
-        if (interval.intervalElapsed()) {
+        inactiveInterval.advance(amount)
+        if (inactiveInterval.intervalElapsed()) {
 
             var manager = Global.getCombatEngine().getFleetManager(ship!!.owner)
             var taskManager = manager.getTaskManager(ship!!.isAlly)
             var assignment = taskManager.getAssignmentFor(ship!!)
 
-            var nearbyShipsIterator = Global.getCombatEngine().shipGrid.getCheckIterator(ship!!.location, 2500f, 2500f)
+            var nearbyShipsIterator = Global.getCombatEngine().shipGrid.getCheckIterator(ship!!.location, 2800f, 2800f)
             var nearbyShips = ArrayList<ShipAPI>()
             nearbyShipsIterator.forEach { if((it as ShipAPI).isVisibleToSide(ship!!.owner)) {nearbyShips.add(it as ShipAPI) } }
 
             var hasNearbyAlly = nearbyShips.any { it.owner == ship!!.owner && it.isAlive && it != ship}
-            var hasNearbyHostile = nearbyShips.any { it.owner == ship!!.owner && it.isAlive && it != ship }
+            var hasNearbyHostile = nearbyShips.any { it.owner != ship!!.owner && it.isAlive && it != ship }
 
             var allShips = Global.getCombatEngine().ships
 
@@ -180,9 +196,17 @@ class HypatiaSystemAI : ShipSystemAIScript {
                 }
 
                 if (targetEntity != null) {
-                    ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MANEUVER_TARGET, 3f, targetEntity!!.location)
+                    /*var offset = getOffset(targetEntity!!)*/
+                    ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MANEUVER_TARGET, 1f, targetEntity!!.location)
+                    //ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MANEUVER_TARGET, 1.25f, targetEntity!!.location)
+
+                    //otherwise those fuckers dont wanna turn early enough
+                    //var angle = Misc.getAngleInDegrees(ship!!.location, targetEntity!!.location)
+                    var angle = Misc.getAngleInDegrees(ship!!.location, targetEntity!!.location)
+                    ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.FACING_OVERRIDE_FOR_MOVE_AND_ESCORT_MANEUVERS, 1f, angle)
+
                     //ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MOVEMENT_DEST, 5f, targetEntity!!.location)
-                    var isInArc = Misc.isInArc(ship!!.facing, 70f, ship!!.location, targetEntity!!.location)
+                    var isInArc = Misc.isInArc(ship!!.facing, 45f, ship!!.location, targetEntity!!.location)
                         if (isInArc) {
                         shouldWarp = true
                     }
@@ -202,52 +226,90 @@ class HypatiaSystemAI : ShipSystemAIScript {
                         clearAssignmentTime = clearAssignmentTimeMax
                         warpTime = maximumWarpTime
                         previousTarget = targetEntity
-                    }
 
-                }
-
-            } else {
-
-                var shouldStop = false
-
-                if (warpTime <= 0) {
-                    shouldStop = true
-                }
-
-                if (targetEntity != null) {
-                    var distance = MathUtils.getDistance(ship, targetEntity)
-                    if (distance <= distanceForUnwarp) {
-                        reachedTarget = true
-                    }
-
-                    if (shouldStop && distance >= distanceForUnwarp * 1.4f) {
-                        shouldStop = true
-                    }
-
-                    if (reachedTarget) {
-                        shouldStop = true
-
+                       // angleAtStart = Misc.getAngleInDegrees(ship!!.location, targetEntity!!.location)
 
                     }
-                }
 
-                //Add some code to prevent unwarps if a hostile ship is in the immediate surrounding
-                var nearbyShipsIterator = Global.getCombatEngine().shipGrid.getCheckIterator(ship!!.location, 1000f, 1000f)
-                var nearbyShips = ArrayList<ShipAPI>()
-                nearbyShipsIterator.forEach { nearbyShips.add(it as ShipAPI) }
-
-                if (nearbyShips.any { MathUtils.getDistance(ship, it) <= 150  && it != ship}) {
-                    shouldStop = false
-                }
-
-                if (shouldStop) {
-                    warpTime = maximumWarpTime
-                    targetEntity = null
-                    reachedTarget = false
-                    ship!!.useSystem()
                 }
 
             }
         }
+
+
+
+        activeInterval.advance(amount)
+        if (activeInterval.intervalElapsed()) {
+
+        }
+
+        //Can not interval this part, certain flags get overwritten every frame (specificly while near an opponent), which breaks the movement code
+        if (ship!!.system.isActive) {
+
+            var shouldStop = false
+
+            if (warpTime <= 0) {
+                shouldStop = true
+            }
+
+            if (targetEntity != null) {
+
+                //var offset = getOffset(targetEntity!!)
+                ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MANEUVER_TARGET, 1f, targetEntity!!.location)
+                ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.BIGGEST_THREAT, 1f, null)
+
+
+                //ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.MANEUVER_TARGET, 1.25f, targetEntity!!.location)
+
+                //otherwise those fuckers dont wanna turn early enough
+                //var angle = Misc.getAngleInDegrees(ship!!.location, targetEntity!!.location)
+                var angle = Misc.getAngleInDegrees(ship!!.location, targetEntity!!.location)
+                ship!!.aiFlags.setFlag(ShipwideAIFlags.AIFlags.FACING_OVERRIDE_FOR_MOVE_AND_ESCORT_MANEUVERS, 1f, angle)
+
+                var distance = MathUtils.getDistance(ship, targetEntity!!.location)
+                if (distance <= distanceForUnwarp) {
+                    reachedTarget = true
+                }
+
+                if (distance > distanceForUnwarp * 1.2f) {
+                    var isInArc = Misc.isInArc(ship!!.facing, 10f, ship!!.location, targetEntity!!.location)
+                    if (ship!!.shipAI != null && isInArc) {
+                        ship!!.blockCommandForOneFrame(ShipCommand.TURN_LEFT)
+                        ship!!.blockCommandForOneFrame(ShipCommand.TURN_RIGHT)
+                    }
+                }
+
+                if (reachedTarget && distance >= distanceForUnwarp * 1.3f) {
+                    shouldStop = true
+                }
+
+                if (reachedTarget) {
+                    shouldStop = true
+                }
+            }
+
+            //Add some code to prevent unwarps if a hostile ship is in the immediate surrounding
+            var nearbyShipsIterator = Global.getCombatEngine().shipGrid.getCheckIterator(ship!!.location, 1000f, 1000f)
+            var nearbyShips = ArrayList<ShipAPI>()
+            nearbyShipsIterator.forEach { nearbyShips.add(it as ShipAPI) }
+
+            if (nearbyShips.any { MathUtils.getDistance(ship, it) <= 50 && it != ship}) {
+                shouldStop = false
+            }
+
+            if (shouldStop) {
+                warpTime = maximumWarpTime
+                targetEntity = null
+                reachedTarget = false
+                /* radialOffset = MathUtils.getRandomNumberInRange(-35f, 35f)
+                 distanceOffset = MathUtils.getRandomNumberInRange(200f, 800f)*/
+                ship!!.useSystem()
+            }
+        }
     }
+
+   /* fun getOffset(target: CombatEntityAPI) : Vector2f {
+        var point = MathUtils.getPointOnCircumference(target.location, distanceOffset, angleAtStart+radialOffset)
+        return point
+    }*/
 }
