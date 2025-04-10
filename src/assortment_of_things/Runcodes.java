ce18eb38eb6c63c13a9be05ca643c306ce3081c5
Lukas22041 <lukas.kurth04@gmail.com>
2025-04-10 19:10:24 +0200

    stuff

--- src/assortment_of_things/Runcodes.java
+++ src/assortment_of_things/Runcodes.java
@@ -1,46 +1,15 @@
 package assortment_of_things;
 
 
-
 import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.AICoreOfficerPlugin;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
-import com.fs.starfarer.api.campaign.PlanetAPI;
-import com.fs.starfarer.api.campaign.PlanetSpecAPI;
-import com.fs.starfarer.api.campaign.econ.MarketAPI;
-import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.impl.campaign.AICoreOfficerPluginImpl;
-import com.fs.starfarer.api.impl.campaign.ids.Commodities;
-import com.fs.starfarer.api.impl.campaign.ids.Industries;
-import com.fs.starfarer.api.ui.TooltipMakerAPI;
-import com.fs.starfarer.loading.specs.PlanetSpec;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.lazywizard.console.Console;
-
-import java.awt.*;
-
+import kotlin.jvm.JvmSerializableLambda;
+import org.lwjgl.util.vector.Vector2f;
 
 public class Runcodes  {
 
     public void Example() {
 
-        /*InteractionDialogAPI dialog;
 
-        dialog.getTextPanel().setFontSmallInsignia();
-        dialog.getTextPanel().addPara("Information Here");
-        dialog.getTextPanel().setFontInsignia();*/
-
-       /* runcode import java.awt.*;
-        import java.awt.datatransfer.StringSelection;
-        Toolkit.getDefaultToolkit()
-                .getSystemClipboard()
-                .setContents(
-                        new StringSelection(Global.getSector().getSeedString()),
-                        null
-                );
-        */
     }
-
 
 }
