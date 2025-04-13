0862cce31db4c96a3e5c0ee779a3e8fc5c781a57
Lukas22041 <lukas.kurth04@gmail.com>
2025-04-13 03:40:06 +0200

    a

--- src/assortment_of_things/Runcodes.java
+++ src/assortment_of_things/Runcodes.java
@@ -2,13 +2,46 @@ package assortment_of_things;
 
 
 import com.fs.starfarer.api.Global;
-import kotlin.jvm.JvmSerializableLambda;
-import org.lwjgl.util.vector.Vector2f;
+import com.fs.starfarer.api.campaign.CampaignEventListener;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.listeners.FleetEventListener;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class Runcodes  {
 
     public void Example() {
 
+
+/*        import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignEventListener;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.listeners.FleetEventListener;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+        Logger log = Global.getLogger(SectorAPI.class);
+        log.setLevel(Level.ALL);
+
+
+        log.debug("");
+        log.debug("CampaignEventListeners");
+        for (CampaignEventListener listener : Global.getSector().getAllListeners()) {
+            log.debug("Event Listener: " + listener.getClass().getName());
+        }
+
+        for (CampaignFleetAPI fleet : Global.getSector().getPlayerFleet().getContainingLocation().getFleets()) {
+
+            log.debug("");
+            log.debug("Fleet: " + fleet.getName());
+
+            for (FleetEventListener listener : fleet.getEventListeners()) {
+                log.debug("Fleet Listener: " + listener.getClass().getName());
+            }
+
+        }*/
 
     }
 
